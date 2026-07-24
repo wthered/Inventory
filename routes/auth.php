4e991db1182982c0cbffe5c35ae295ed002936b7
wthered <wthered@gmail.com>
2026-07-24 08:51:51 +0300

    Fixed Auth routes

--- routes/auth.php
+++ routes/auth.php
@@ -4,14 +4,12 @@
 	use App\Http\Controllers\Auth\PasswordController;
 	use Illuminate\Support\Facades\Route;
 
-		Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('auth.sign.up');
-		Route::post('/register', [AuthController::class, 'register'])->name('register');
-		Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-		Route::post('/login', [AuthController::class, 'login'])->name('auth.sign.in');
+	Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('auth.sign.up');
+	Route::post('/register', [AuthController::class, 'register'])->name('register');
+	Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+	Route::post('/login', [AuthController::class, 'login'])->name('auth.sign.in');
 
-		Route::get('/forgot-password', [PasswordController::class, 'requestPasswordReset'])->name('password.request');
-		Route::post('/forgot-password', [PasswordController::class, 'emailPasswordReset'])->name('password.email');
-		Route::get('/reset-password/{token}', [PasswordController::class, 'resetPassword'])->name('password.reset');
-		Route::post('/reset-password', [PasswordController::class, 'updatePassword'])->name('password.update');
-
-	});
+	Route::get('/forgot-password', [PasswordController::class, 'requestPasswordReset'])->name('password.request');
+	Route::post('/forgot-password', [PasswordController::class, 'emailPasswordReset'])->name('password.email');
+	Route::get('/reset-password/{token}', [PasswordController::class, 'resetPassword'])->name('password.reset');
+	Route::post('/reset-password', [PasswordController::class, 'updatePassword'])->name('password.update');
